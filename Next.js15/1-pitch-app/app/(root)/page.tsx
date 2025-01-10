112fcbf4cc80a52d6001821d4a84c14b47229c5c
fabioono25 <fabioono25@gmail.com>
2025-01-11 00:22:48 +0100

    adding card for list + allowing SGV

--- Next.js15/1-pitch-app/app/(root)/page.tsx
+++ Next.js15/1-pitch-app/app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import SearchForm from "@/components/SearchForm";
-// import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 // import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 // import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 // import { auth } from "@/auth";
@@ -17,6 +17,31 @@ export default async function Home({
   // console.log(session?.id);
 
   // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  const posts = [
+    {
+      _createdAt: "2021-09-01T00:00:00Z",
+      views: 12,
+      author: { _id: 1, name: "John Doe", image: 'https://placehold.co/48x48' },
+      _id: 1,
+      description: "This is a description",
+      title: "Startup Title",
+      category: "Tech",
+      image:
+        "https://plus.unsplash.com/premium_photo-1736437251499-9b5d6f0a9a53?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _createdAt: "2021-09-01T00:00:00Z",
+      views: 12,
+      author: { _id: 1, name: "John Doe", image: 'https://placehold.co/48x48' },
+      _id: 2,
+      description: "This is a description",
+      title: "Startup Title",
+      category: "Tech",
+      image:
+        "https://plus.unsplash.com/premium_photo-1736437251499-9b5d6f0a9a53?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
 
   return (
     <>
@@ -39,7 +64,7 @@ export default async function Home({
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
 
-        {/* <ul className="mt-7 card_grid">
+        <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
@@ -47,7 +72,7 @@ export default async function Home({
           ) : (
             <p className="no-results">No startups found</p>
           )}
-        </ul> */}
+        </ul>
       </section>
 
       {/* <SanityLive /> */}
