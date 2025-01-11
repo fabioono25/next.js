f4b1ca384f1de8f1760fe85e5b204c605474aef5
fabioono25 <fabioono25@gmail.com>
2025-01-11 12:13:15 +0100

    generating types for TS, getting data

--- Next.js15/1-pitch-app/app/(root)/page.tsx
+++ Next.js15/1-pitch-app/app/(root)/page.tsx
@@ -1,7 +1,8 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 // import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-// import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { sanityFetch } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 // import { auth } from "@/auth";
 
 export default async function Home({
@@ -16,32 +17,34 @@ export default async function Home({
 
   // console.log(session?.id);
 
-  // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
-  const posts = [
-    {
-      _createdAt: "2021-09-01T00:00:00Z",
-      views: 12,
-      author: { _id: 1, name: "John Doe", image: 'https://placehold.co/48x48' },
-      _id: 1,
-      description: "This is a description",
-      title: "Startup Title",
-      category: "Tech",
-      image:
-        "https://plus.unsplash.com/premium_photo-1736437251499-9b5d6f0a9a53?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      _createdAt: "2021-09-01T00:00:00Z",
-      views: 12,
-      author: { _id: 1, name: "John Doe", image: 'https://placehold.co/48x48' },
-      _id: 2,
-      description: "This is a description",
-      title: "Startup Title",
-      category: "Tech",
-      image:
-        "https://plus.unsplash.com/premium_photo-1736437251499-9b5d6f0a9a53?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
+  console.log(JSON.stringify(posts, null, 2));
+
+  // const posts = [
+  //   {
+  //     _createdAt: "2021-09-01T00:00:00Z",
+  //     views: 12,
+  //     author: { _id: 1, name: "John Doe", image: 'https://placehold.co/48x48' },
+  //     _id: 1,
+  //     description: "This is a description",
+  //     title: "Startup Title",
+  //     category: "Tech",
+  //     image:
+  //       "https://plus.unsplash.com/premium_photo-1736437251499-9b5d6f0a9a53?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //   },
+  //   {
+  //     _createdAt: "2021-09-01T00:00:00Z",
+  //     views: 12,
+  //     author: { _id: 1, name: "John Doe", image: 'https://placehold.co/48x48' },
+  //     _id: 2,
+  //     description: "This is a description",
+  //     title: "Startup Title",
+  //     category: "Tech",
+  //     image:
+  //       "https://plus.unsplash.com/premium_photo-1736437251499-9b5d6f0a9a53?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //   },
+  // ];
 
   return (
     <>
