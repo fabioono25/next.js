00f9b0706e09492e5136762192061180a026bfe2
fabioono25 <fabioono25@gmail.com>
2025-01-16 09:40:53 +0100

    adding authentication/creating of user in Sanity

--- Next.js15/1-pitch-app/app/(root)/page.tsx
+++ Next.js15/1-pitch-app/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 // import { client } from "@/sanity/lib/client";
@@ -14,9 +15,9 @@ export default async function Home({
   const query = (await searchParams).query;
   const params = { search: query || null };
 
-  // const session = await auth();
+  const session = await auth();
 
-  // console.log(session?.id);
+  console.log(session?.id);
 
   // const posts = await client.fetch(STARTUPS_QUERY);
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
