c80d3a4b2ddb13131898c50429179f12c225e201
fabioono25 <fabioono25@gmail.com>
2025-01-18 13:11:58 +0100

    editor's choice

--- Next.js15/1-pitch-app/app/(root)/startup/[id]/page.tsx
+++ Next.js15/1-pitch-app/app/(root)/startup/[id]/page.tsx
@@ -1,20 +1,35 @@
-import { formatDate } from "@/lib/utils";
-import Image from "next/image";
-import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import Link from "next/link";
-import markdownit from "markdown-it";
 import { Suspense } from "react";
+import { client } from "@/sanity/lib/client";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
+import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+
+const md = markdownit();
 
 export const experimental_ppr = true;
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
-  // console.log("post:", post);
-  const md = markdownit();
+
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
+
+  if (!post) return notFound();
+
   const parsedContent = md.render(post?.pitch || "");
 
   return (
@@ -25,15 +40,14 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
+
       <section className="section_container">
-        <Image
+        <img
           src={post.image}
           alt="thumbnail"
-          layout="responsive"
-          width={700}
-          height={475}
-          className="rounded-xl"
+          className="w-full h-auto rounded-xl"
         />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
@@ -69,9 +83,21 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
             <p className="no-result">No details provided</p>
           )}
         </div>
+
         <hr className="divider" />
 
-        {/* Adding the dynamic capability */}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense>
